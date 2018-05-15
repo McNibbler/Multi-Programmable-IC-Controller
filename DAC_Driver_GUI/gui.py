41a23a1e40430e5717f6eeffe4864b05e1695291
McNibbler <McNibbler@users.noreply.github.com>
2018-05-15 09:25:18 -0400

    further UI enhancements. need to implement backend soon

--- DAC_Driver_GUI/gui.py
+++ DAC_Driver_GUI/gui.py
@@ -65,6 +65,7 @@ class Application(QWidget):
         self.voltage_slider_a = QSlider(Qt.Horizontal)
         self.voltage_slider_a.setRange(min(self.bipolar_range), max(self.bipolar_range))
         self.voltage_slider_a.valueChanged[int].connect(self.change_voltage)
+        self.voltage_slider_a.sliderReleased.connect(self.send_slider)
 
         self.voltage_label_b = QLabel()
         self.voltage_label_b.setText('DAC B')
@@ -74,6 +75,7 @@ class Application(QWidget):
         self.voltage_slider_b = QSlider(Qt.Horizontal)
         self.voltage_slider_b.setRange(min(self.bipolar_range), max(self.bipolar_range))
         self.voltage_slider_b.valueChanged[int].connect(self.change_voltage)
+        self.voltage_slider_b.sliderReleased.connect(self.send_slider)
 
         # Input text boxes for voltage
         self.only_double = QDoubleValidator()
@@ -94,10 +96,11 @@ class Application(QWidget):
         self.setWindowTitle('DAC Controller')
 
         self.is_bipolar = True
+        self.is_tied = False
 
         self.main_window()
 
-    # Main window execution
+    # Main window execution and layout
     def main_window(self):
         # Layout
         layout = QGridLayout()
@@ -128,7 +131,20 @@ class Application(QWidget):
 
     # Ties the two sliders together
     def tie_outputs(self):
-        pass
+
+        if self.is_tied:
+            self.is_tied = False
+
+            self.voltage_textbox_b.setEnabled(True)
+            self.voltage_slider_b.setEnabled(True)
+            self.voltage_label_b.setText('DAC B')
+
+        else:
+            self.is_tied = True
+
+            self.voltage_textbox_b.setEnabled(False)
+            self.voltage_slider_b.setEnabled(False)
+            self.voltage_label_b.setText('DAC B (Tied to DAC A)')
 
     # Select between bipolar mode and unipolar
     def bipolar_toggle(self):
@@ -149,6 +165,7 @@ class Application(QWidget):
 
     # Handler for when the slider is changed
     def change_voltage(self):
+
         new_voltage_a = self.voltage_slider_a.value() / self.iterator
         new_voltage_b = self.voltage_slider_b.value() / self.iterator
 
@@ -157,6 +174,7 @@ class Application(QWidget):
 
     # Handler for when the text is changed and the sliders need to be updated
     def update_sliders(self):
+
         new_voltage_a = float(self.voltage_textbox_a.text())
         new_voltage_b = float(self.voltage_textbox_b.text())
 
@@ -172,9 +190,27 @@ class Application(QWidget):
         self.voltage_slider_a.setValue(int(new_voltage_a * self.iterator))
         self.voltage_slider_b.setValue(int(new_voltage_b * self.iterator))
 
+        if self.is_tied:
+            # WRITE CODE HERE TO SEND VOLTAGE TO THE DACS THROUGH THE 'BOTH' REGISTER
+            pass
+        else:
+            # WRITE CODE HERE TO SEND VOLTAGE TO THE DACS THROUGH THE INDIVIDUAL REGISTERS
+            pass
+
     # Sends the setup command to the DAC
     def setup(self):
+        # WRITE CODE HERE TO SEND THE SETUP PROCESS TO THE DAC
         pass
+
+    # Sends update commands to the DAC upon releasing the slider
+    def send_slider(self):
+        if self.is_tied:
+            # WRITE CODE HERE TO SEND VOLTAGE TO THE DACS THROUGH THE 'BOTH' REGISTER
+            pass
+        else:
+            # WRITE CODE HERE TO SEND VOLTAGE TO THE DACS THROUGH THE INDIVIDUAL REGISTERS
+            pass
+
 
 ###################################################
 
